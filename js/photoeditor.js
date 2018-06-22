fc5360b1dd092980b2316d12ee10c1ad9089e974
Saket Kumar <saki007ster@gmail.com>
2018-06-22 15:33:32 +0530

    adding styles to the wysiwyg for default styles of the text block.

--- js/photoeditor.js
+++ js/photoeditor.js
@@ -15,7 +15,7 @@ $(function() {
   function addTextBlock (cnt) {
     cnt = cnt + 1;
     var cta_cnt = 0;
-		var textBlockEditor = '<div class="text-block-editor-' + cnt + '"><div class="col s4"><div id="toolbar-' + cnt + '"><a data-wysihtml5-command="bold">B</a><a data-wysihtml5-command="italic">I</a><a data-wysihtml5-command="underline">U</a><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1">H1</a><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="p">P</a><a data-wysihtml-command="foreColor" data-wysihtml-command-value="red">red</a><a data-wysihtml-command="foreColor" data-wysihtml-command-value="green">green</a><a data-wysihtml-command="foreColor" data-wysihtml-command-value="blue">blue</a></div><div class="add-cta-grp"><a id="add-cta" class="waves-effect waves-light btn-small">Add CTA</a><a id="remove-cta" class="waves-effect waves-light btn-small">Remove CTA</a></div></div><div class="col s4"><div class="promo-block-position"><h2>Text Block Position</h2><div class="form-group"><label for="top">Position From Top</label><input type="range" value="50" min="1" max="100" step="1" id="top-position"></div><div class="form-group"><label for="left">Position From Left</label><input type="range" value="50" min="1" max="100" step="1" id="left-position"></div></div></div><div class="col s4"><h2>Text Block Properties</h2><div class="form-group"><label for="bg-color">BG Color</label><input type="color" value="" id="bg-color" size="10"></div><div class="form-group"><label for="border-color">Border color</label><input type="color" value="" id="border-color"></div><div class="form-group"><label for="border-size">Border Size</label><input type="range" value="0" min="0" max="10" step="1" id="border-size"></div><div class="form-group"><label for="spacing">Spacing </label><input type="range" value="10" min="1" max="120" step="1" id="spacing"></div></div></div>';
+		var textBlockEditor = '<div class="text-block-editor-' + cnt + '"><div class="col s4"><div id="toolbar-' + cnt + '"><a data-wysihtml5-command="bold">B</a><a data-wysihtml5-command="italic">I</a><a data-wysihtml5-command="underline">U</a><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1">H1</a><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="p">P</a><a data-wysihtml-command="foreColor" data-wysihtml-command-value="red">red</a><a data-wysihtml-command="foreColor" data-wysihtml-command-value="green">green</a><a data-wysihtml-command="foreColor" data-wysihtml-command-value="blue">blue</a></div><div class="add-cta-grp"><a id="add-cta" class="waves-effect waves-light btn-small">Add CTA</a><a id="remove-cta" class="waves-effect waves-light btn-small">Remove CTA</a></div><div class="styles-grp"><a id="add-style-black" class="waves-effect waves-light btn-small">Dark Style</a><a id="add-style-white" class="waves-effect waves-light btn-small">Light Style</a></div></div><div class="col s4"><div class="promo-block-position"><h2>Text Block Position</h2><div class="form-group"><label for="top">Position From Top</label><input type="range" value="50" min="1" max="100" step="1" id="top-position"></div><div class="form-group"><label for="left">Position From Left</label><input type="range" value="50" min="1" max="100" step="1" id="left-position"></div></div></div><div class="col s4"><h2>Text Block Properties</h2><div class="form-group"><label for="bg-color">BG Color</label><input type="color" value="" id="bg-color" size="10"></div><div class="form-group"><label for="border-color">Border color</label><input type="color" value="" id="border-color"></div><div class="form-group"><label for="border-size">Border Size</label><input type="range" value="0" min="0" max="10" step="1" id="border-size"></div><div class="form-group"><label for="spacing">Spacing </label><input type="range" value="10" min="1" max="120" step="1" id="spacing"></div></div></div>';
 		$('.promo-editor').append(textBlockEditor);
 		
 		var textId = 'editor-' + cnt;
@@ -25,11 +25,30 @@ $(function() {
 			toolbar: toolbarId,
 			parserRules:  wysihtml5ParserRules
     });
+
+    $('.text-block-editor-' + cnt + ' #add-style-black').on('click', function(){
+      if($('#' + textId).hasClass('style-white')) {
+        $('#' + textId).addClass('style-black');
+        $('#' + textId).removeClass('style-white');
+      } else {
+        $('#' + textId).addClass('style-black');
+      }
+    });
+
+    $('.text-block-editor-' + cnt + ' #add-style-white').on('click', function(){
+      if($('#' + textId).hasClass('style-black')) {
+        $('#' + textId).addClass('style-white');
+        $('#' + textId).removeClass('style-black');
+      } else {
+        $('#' + textId).addClass('style-white');
+      }
+    });
     
     $('.text-block-editor-' + cnt + ' #add-cta').on('click', function(){
       cta_cnt = cta_cnt + 1;
       $('#' + textId).append('<button class="cta-'+ cta_cnt +'">CTA BUTTON</button>');
     });
+
     $('.text-block-editor-' + cnt + ' #remove-cta').on('click', function(){
       $('#' + textId).find('.cta-'+ cta_cnt).remove();
       cta_cnt = cta_cnt - 1;
